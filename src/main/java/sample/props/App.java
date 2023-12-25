410017a50f9d15b8fccbe347fc7ed1e662aa9eae
SampleUser0001 <ittimfn+github@gmail.com>
2023-12-25 23:48:49 +0900

    update package name properties -> props

--- src/main/java/sample/props/App.java
+++ src/main/java/sample/props/App.java
@@ -1,4 +1,4 @@
-package sample.properties;
+package sample.props;
 
 import java.util.Properties;
 import java.io.InputStream;
@@ -10,12 +10,15 @@ import java.nio.file.Paths;
  * Mavenのresources配下のpropertiesを読み込む
  */
 public class App {
-    public static final String PROPERTIES_FILEPATH = Paths.get("/","properties","sample.properties").toString();
+    public static final String PROPERTIES_FILEPATH = Paths.get("/", "properties", "sample.properties").toString();
     public static final String SAMPLE_KEY = "sample.key";
     
     public void exec(String[] args) {
+        System.out.println("PROPERTIES_FILEPATH : " + PROPERTIES_FILEPATH);
         Properties props = new Properties();
-        try (InputStream in = getClass().getResourceAsStream(PROPERTIES_FILEPATH)){
+        Class clazz = getClass();
+        System.out.println("class : " + clazz);
+        try (InputStream in = clazz.getResourceAsStream(PROPERTIES_FILEPATH)){
             props.load(new InputStreamReader(in, "UTF-8"));
         } catch (IOException e) {
             e.printStackTrace();
