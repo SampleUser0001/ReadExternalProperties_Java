e44cc6413efdc628dc7e495f373ded8036004cb3
SampleUser0001 <ittimfn+github@gmail.com>
2023-12-26 00:57:07 +0900

    exec:javaで読み込みはできる。jarファイルはsample.propertiesを見つけられない

--- src/main/java/sample/props/App.java
+++ src/main/java/sample/props/App.java
@@ -1,6 +1,7 @@
 package sample.props;
 
 import java.util.Properties;
+import java.util.ResourceBundle;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -10,15 +11,13 @@ import java.nio.file.Paths;
  * Mavenのresources配下のpropertiesを読み込む
  */
 public class App {
-    public static final String PROPERTIES_FILEPATH = Paths.get("/", "properties", "sample.properties").toString();
+    public static final String PROPERTIES_FILEPATH = Paths.get("sample.properties").toString();
     public static final String SAMPLE_KEY = "sample.key";
     
     public void exec(String[] args) {
         System.out.println("PROPERTIES_FILEPATH : " + PROPERTIES_FILEPATH);
         Properties props = new Properties();
-        Class clazz = getClass();
-        System.out.println("class : " + clazz);
-        try (InputStream in = clazz.getResourceAsStream(PROPERTIES_FILEPATH)){
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILEPATH)){
             props.load(new InputStreamReader(in, "UTF-8"));
         } catch (IOException e) {
             e.printStackTrace();
@@ -26,9 +25,15 @@ public class App {
         System.out.println(props.getProperty(SAMPLE_KEY));
     }
 
+    public void readByBundle(String[] args) {
+        ResourceBundle res = ResourceBundle.getBundle("sample");
+        System.out.println(res.getString(SAMPLE_KEY));
+    }
+
     
     public static void main(String[] args) {
         new App().exec(args);
+        // new App().readByBundle(args);
     }
 
 }
